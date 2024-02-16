f1562afef4942e4a814e5582973f0bbce4dce1f3
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-02-16 02:27:36 +0100

    update

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-  }
-  
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+var currentYear = new Date().getFullYear();
+
+// Update the content of the span element with id "currentYear" with the current year
+document.getElementById("currentYear").textContent = currentYear;
